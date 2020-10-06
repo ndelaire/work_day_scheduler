cd8a7ef4b6c496c30a7e3e4ed8449a68ee44dfd6
Nadine Delaire <ndelaire16@gmail.com>
2020-10-05 18:27:54 -0700

    updating JS

--- planner.js
+++ planner.js
@@ -10,7 +10,18 @@ $(document).ready(function(){
 
     }); 
     
+    $("#7 .description").val(localStorage.getItem("7"));
+    $("#8 .description").val(localStorage.getItem("8"));
     $("#9 .description").val(localStorage.getItem("9"));
+    $("#10 .description").val(localStorage.getItem("10"));
+    $("#11 .description").val(localStorage.getItem("11"));
+    $("#12 .description").val(localStorage.getItem("12"));
+    $("#13 .description").val(localStorage.getItem("13"));
+    $("#14 .description").val(localStorage.getItem("14"));
+    $("#15 .description").val(localStorage.getItem("15"));
+    $("#16 .description").val(localStorage.getItem("16"));
+    $("#17 .description").val(localStorage.getItem("17"));
+    $("#18 .description").val(localStorage.getItem("18"));
     // add more rows for more time
     function timeChange(){
         var currentHour = moment().hour(); // Number
